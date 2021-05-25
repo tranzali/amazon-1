3806db172bed7943dc4d15dcac14e30eb79d02f0
tranzali <qarobar3@gmail.com>
2021-05-25 21:06:39 +0100

    finished auth, working on checkout page

---
+++ firebase.js
@@ -0,0 +1,10 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBPG4wAByRwYxWErOFIaz4gTJxJrA9YEKo",
+    authDomain: "amzn-1.firebaseapp.com",
+    projectId: "amzn-1",
+    storageBucket: "amzn-1.appspot.com",
+    messagingSenderId: "459265072236",
+    appId: "1:459265072236:web:1e7b28e8f47dc52a310c49"
+  };
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,13 +4,19 @@ import {
     SearchIcon,
     ShoppingCartIcon,
 } from "@heroicons/react/outline"
+import { signIn, signOut, useSession } from "next-auth/client"
+import { useRouter } from "next/router"
 
 function Header() {
+    const [session] = useSession();
+    const router = useRouter();
+
     return (
-        <header>
+        <header className="sticky top-0 z-50">
             <div className="flex items-center bg-amazon_blue p-1 flex-grow py-2">
                 <div className="mt-2 flex items-center flex-grow sm:flex-grow-0">
                     <Image 
+                        onClick={() => router.push('/')}
                         src="https://links.papareact.com/f90"
                         width={150}
                         height={40}
@@ -27,15 +33,15 @@ function Header() {
 
                 {/* Right */}
                 <div className="flex items-center text-xs space-x-6 mx-6 whitespace-nowrap text-white">
-                    <div className="link">
-                        <p>Hello CoderJoe</p>
+                    <div onClick={!session ? signIn : signOut} className="link">
+                        <p>{session ? `Hello, ${session.user.name}` : `Sign In`}</p>
                         <p className="font-extrabold md:text-sm">Account & Lists</p>
                     </div>
                     <div className="link">
                         <p>Returns</p>
                         <p className="font-extrabold md:text-sm">& Orders</p>
                     </div>
-                    <div className="relative link flex items-center">
+                    <div onClick={() => router.push('/checkout')} className="relative link flex items-center">
                         <span className="absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold">0</span>
                         <ShoppingCartIcon className="h-10" />
                         <p className="hidden md:inline font-extrabold md:text-sm mt-2">
