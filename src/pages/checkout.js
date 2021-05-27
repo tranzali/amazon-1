5609d3f0d047b61e948456a6aefe6b51b4e3b824
tranzali <qarobar3@gmail.com>
2021-05-27 21:17:43 +0100

    checkout page complete

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -1,7 +1,16 @@
 import Image from "next/image"
+import { useSelector } from "react-redux"
+import CheckoutProduct from "../components/CheckoutProduct"
 import Header from "../components/Header"
+import { selectItems, selectTotal } from "../slices/basketSlice"
+import Currency from "react-currency-formatter"
+import { useSession } from "next-auth/client"
 
 function Checkout() {
+    const items = useSelector(selectItems)
+    const total = useSelector(selectTotal)
+    const [session] = useSession()
+
     return (
         <div className="bg-gray-100">
             <Header />
@@ -16,12 +25,45 @@ function Checkout() {
                         objectFit="contain"
                     />
                     <div className="flex flex-col p-5 space-y-10 bg-white">
-                        <h1 className="text-3xl border-b pb-4">Your Shopping Basket</h1>
+                        <h1 className="text-3xl border-b pb-4">
+                            {items.length === 0 ? `Your Amazon Basket is Empty` :`Your Shopping Basket`}
+                        </h1>
+
+                        {items.map((item, i) => (
+                            <CheckoutProduct 
+                                key={i}
+                                id={item.id}
+                                title={item.title}
+                                price={item.price}
+                                rating={item.rating}
+                                description={item.description}
+                                category={item.category}
+                                image={item.image}
+                                hasPrime={item.hasPrime}
+                            />
+                        ))}
                     </div>
                 </div>
 
                 {/* right */}
-                <div></div>
+                <div className="flex flex-col bg-white p-10 shadow-md">
+                    {items.length > 0 && (
+                        <>
+                            <h2 className="font-bold whitespace-nowrap">Subtotal ({items.length} items): {" "}
+                                <span>
+                                    <Currency quantity={total} currency="GBP" />
+                                </span>
+                            </h2>
+
+                            <button disabled={!session}
+                            className={`button mt-2 ${!session && 
+                            "from-gray-300 to-gray-500 border-gray-200 text-gray-300 cursor-not-allowed"}`}>
+                                {!session ? "Sogn in to checkout" : "Proceed to checkout"}
+                            </button>
+                        </>
+
+                    )}
+                </div>
             </main>
         </div>
     )
